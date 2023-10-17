3b717c66047965817897e00d08807644dfc97472
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2023-10-17 10:58:06 -0300

    main.js

---
+++ images/main.js
@@ -0,0 +1,9 @@
+//Criando/ddeclarando a função tocaSomPom
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+//chamando/invocando a função tocaSomPom pelo clique do botão
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
