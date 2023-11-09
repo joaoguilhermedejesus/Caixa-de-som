16edce01bea24fbf191271dad6e1382e577e97e4
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2023-11-08 22:37:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-//Criando/ddeclarando a função tocaSom com parâmetro idElementoAudio
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+//Criando/declarando a função tocaSom com parâmetro idElementoAudio
+function tocaSom(idElementoAudio) {
+    document .querySelector(idElementoAudio) .play();
 }
-
-//criando referência constante listaDeTeclas buscando todos os itens pela clase .tecla
+//criando referência constante listaDeTeclas buscando todos os itens pela classe tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//criando referência variável "contador" iniciando = 0
-
-
-//estrutura dee repetição "while" significa "enquanto algo acontecer"
-for(let contador = 0;contador < listaDeTeclas.length;constaor = contador + 1)
-    const tecla = listaDeTeclas [contador]
-    const efeito = tecla.classlist[1]; //acessa o 2° item da classe de classe de teclas
-    const idAudio = `#som_+ ${efeito}`;listaDeTeclas[contador]
-    tecla.onclick = function(){
-        tocaSom(idAudio)
-    }
-    
-
+//estrutura de repetição "while" significa "enquanto algo acontecer" mudou while para "for"
+for(let contador = 0;contador < listaDeTeclas.length;contador++){
+    const tecla = listaDeTeclas[contador]
+    const efeito = tecla.classList[1]; //acessa o 2° item da classe de teclas
+    const idAudio = `#som_${efeito}`; //template string
+    tecla.onclick = function(){ //cria função anônima para chamar tocaSom
+    tocaSom(idAudio);
+}
+}
