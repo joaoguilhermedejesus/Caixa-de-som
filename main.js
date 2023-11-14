af9c324de8bc2eeb53dd4d53bf414e32dbd9b42b
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2023-11-14 11:13:20 -0300

    y

--- main.js
+++ main.js
@@ -12,4 +12,7 @@ for(let contador = 0;contador < listaDeTeclas.length;contador++){
     tecla.onclick = function(){ //cria função anônima para chamar tocaSom
     tocaSom(idAudio);
 }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
 }
